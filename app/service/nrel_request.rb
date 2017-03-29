7c2349d4b88c80a1f1381ce25c4a709ec7ae0874
Ashley Schauer <aelschauer@gmail.com>
2017-03-29 10:29:35 -0600

    adds passing test to query nearest station for NREL request

---
+++ app/service/nrel_request.rb
@@ -0,0 +1,20 @@
+class NrelRequest
+  attr_reader :api_key
+
+  def initialize(api_key)
+    @api_key = api_key
+  end
+
+  def nearest_stations(zip_code)
+    conn = Faraday.new("https://api.data.gov/")
+    response = conn.get "nrel/alt-fuel-stations/v1/nearest.json", {
+      limit: 10,
+      location: zip_code,
+      fuel_type: "ELEC,LPG",
+      api_key: api_key
+    }
+    results = JSON.parse(response.body)
+    results["fuel_stations"]
+  end
+
+end
--- spec/service/nrel_request_spec.rb
+++ spec/service/nrel_request_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe 'NrelRequest' do
-  it '#fuel_stations' do
-    NrelRequest.new(ENV['api_key'])
+  it '#nearest_stations' do
+    request = NrelRequest.new(ENV['api_key'])
+
+    stations = request.nearest_stations("80203")
+
+    expect(stations.count).to eq(10)
+    expect(stations.first["distance"]).to eq(0.3117)
+    expect(stations.last["distance"]).to eq(0.88537)
+
+    stations.each do |station|
+      expect(station["fuel_type_code"]).to be_in(["ELEC", "LPG"])
+    end
+
   end
 end
